0386f92f66b7ea5b0a6a02a433682502d8fe884b
BayuMOD <145831135+BayuMOD@users.noreply.github.com>
2023-12-18 17:24:32 +0700

    Add files via upload

--- websocket/insshws.sh
+++ websocket/insshws.sh
@@ -1,41 +1,17 @@
 #!/bin/bash
-# Proxy For WS
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-clear
-red='\e[1;31m'
-green='\e[0;32m'
-yell='\e[1;33m'
-NC='\e[0m'
-echo "Installing SSH Websocket by KlmpkVPN" | lolcat
-echo "Progress..." | lolcat
-sleep 3
+#installer Websocker tunneling 
+
 cd
 
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/BayuMOD/v4/main/websocket/dropbear-ws.py
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/BayuMOD/v4/main/websocket/ws-stunnel
+#Install Script Websocket-SSH Python
+#wget -O /usr/local/bin/ws-openssh bebejje
+wget -O /usr/local/bin/ws-dropbear -##
+wget -O /usr/local/bin/ws-stunnel 8-2728+383+
+#wget -O /usr/local/bin/ws-ovpn https://raw.githubusercoheheueyehhntent.com/BayuMOD/v4/sshws/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
 
+#izin permision
+#chmod +x /usr/local/bin/ws-openssh
 chmod +x /usr/local/bin/ws-dropbear
-chmod +x /usr/local/bin/ws-stunnel
-
-wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/BayuMOD/v4/main/websocket/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
-
-wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/BayuMOD/v4/main/websocket/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
-
-systemctl daemon-reload
-systemctl enable ws-dropbear.service
-systemctl start ws-dropbear.service
-systemctl restart ws-dropbear.service
-systemctl enable ws-stunnel.service
-systemctl start ws-stunnel.service
-systemctl restart ws-stunnel.service
+chmod +x /usr/local/bin/ws-stunnel 
+egebenenebsy
 
